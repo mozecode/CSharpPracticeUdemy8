3d365e9a379ef4e9bad6bde4b1aa2c3b0a72f83b
Greta Moseley <greta.moseley@gmail.com>
2017-12-20 11:30:58 -0600

    add C# exercise to create max number generator given user-entered string of comma-separated numbers.  Uses split and max.

--- CSharpUdemyPractice8/Program.cs
+++ CSharpUdemyPractice8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace CSharpUdemyPractice8
     {
         static void Main(string[] args)
         {
+            //Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the 
+            //numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program 
+            //should display 8.
+
+            char[] splitChar = { ',' };
+            var sum = 0;
+            Console.WriteLine("Please enter a list of numbers separated by commas:  ");
+            string text = Console.ReadLine();
+
+            Console.WriteLine();//blank
+
+            Console.WriteLine("You entered " + text + ".");
+
+            Console.WriteLine();//blank
+            
+            var numbers= text.Split(splitChar); //creates an array with the numbers?
+
+            var maxValue = numbers.Max();
+
+            Console.WriteLine(maxValue);
+
+
+
+
+
+
+
+
         }
     }
 }
